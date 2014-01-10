6ffc6f4cba6c9ec7f714c7d7dc29705b7ae0f40d
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2014-01-10 11:55:24 +0900

    Adds utility methods in SExprParsers.

--- src/main/scala/org/goldenport/sexpr/eval/SExprParsers.scala
+++ src/main/scala/org/goldenport/sexpr/eval/SExprParsers.scala
@@ -6,13 +6,13 @@ import scala.util.parsing.input._
 
 /*
  * @since   Jan.  9, 2014
- * @version Jan.  9, 2014
+ * @version Jan. 10, 2014
  * @author  ASAMI, Tomoharu
  */
 trait SExprParsers extends Parsers {
   type Elem = SExpr
 
-  def atomName: Parser[String] = new Parser[String] {
+  protected def atom_name: Parser[String] = new Parser[String] {
     def apply(in: Input) = {
       in.first match {
         case SAtom(name) => Success(name, in.rest)
@@ -21,7 +21,7 @@ trait SExprParsers extends Parsers {
     }
   }
 
-  def atom(name: String): Parser[SAtom] = new Parser[SAtom] {
+  protected def atom(name: String): Parser[SAtom] = new Parser[SAtom] {
     def apply(in: Input) = {
       val NAME = name
       in.first match {
@@ -31,7 +31,7 @@ trait SExprParsers extends Parsers {
     }
   }
 
-  def string: Parser[String] = new Parser[String] {
+  protected def string: Parser[String] = new Parser[String] {
     def apply(in: Input) = {
       in.first match {
         case SString(s) => Success(s, in.rest)
@@ -40,7 +40,20 @@ trait SExprParsers extends Parsers {
     }
   }
 
-  def open = new Parser[SPseudo] {
+  protected def number_long: Parser[Long] = new Parser[Long] {
+    def apply(in: Input) = {
+      in.first match {
+        case SNumber(s) => try {
+          Success(s.toLong, in.rest)
+        } catch {
+          case e => Failure("not long = " + e, in.rest)
+        }
+        case x => Failure("not number = " + x, in.rest)
+      }
+    }
+  }
+
+  protected def open = new Parser[SPseudo] {
     def apply(in: Input) = {
       in.first match {
         case SOpen => Success(SOpen, in.rest)
@@ -49,12 +62,95 @@ trait SExprParsers extends Parsers {
     }
   }
 
-  def close = new Parser[SPseudo] {
+  protected def close = new Parser[SPseudo] {
     def apply(in: Input) = {
       in.first match {
         case SClose => Success(SClose, in.rest)
         case _ => Failure("not close", in.rest)
       }
     }
+  }
+
+  protected def atom_string(name: String): Parser[String] = {
+    // XXX handle keywords
+    open ~ atom(name) ~> string <~ close
+  }
+
+  protected def atom_long_list(name: String): Parser[Seq[Long]] = {
+    // XXX handle keywords
+    open ~ atom(name) ~> number_long.* <~ close
+  }
+
+  protected def atom_string_list(name: String): Parser[Seq[String]] = {
+    // XXX handle keywords
+    open ~ atom(name) ~> string.* <~ close
+  }
+
+  protected def atom_expr_list(name: String): Parser[Seq[SExpr]] = {
+    // XXX handle keywords
+    open ~ atom(name) ~> expr_list <~ close ^^ {
+      case exprs => exprs
+    }
+  }
+
+  protected def expr_list = new Parser[Seq[SExpr]] {
+    def apply(in: Input) = {
+      val (r, i) = contents(in)
+      Success(r, i)
+    }
+  }
+
+  def contents(in: Input): (Seq[SExpr], Input) = {
+    var input = in
+    var result = Vector.empty[SExpr]
+    var done = false
+    while (!done) {
+      input.first match {
+        case SOpen => {
+          val (r, i) = element(input.rest)
+          result = result :+ r
+          input = i
+        }
+        case SClose => {
+          done = true
+        }
+        case x => {
+          result = result :+ x
+          input = input.rest
+        }
+      }
+    }
+    (result, input)
+  }
+
+  def element(in: Input): (SExpr, Input) = {
+    var input = in
+    var result = Vector.empty[SExpr]
+    var done = false
+    while (!done) {
+      input.first match {
+        case SOpen => {
+          val (r, i) = element(input.rest)
+          result = result :+ r
+          input = i
+        }
+        case SClose => {
+          done = true
+          input = input.rest
+        }
+        case x => {
+          result = result :+ x
+          input = input.rest
+        }
+      }
+    }
+    if (result.isEmpty)
+      (SNil, input)
+    else
+      (make_list(result), input)
+  }
+
+  protected def make_list(xs: Seq[SExpr]): SList = {
+    xs.foldRight(SNil: SList) { (x, z) => SCell(x, z) }
   }
 }
