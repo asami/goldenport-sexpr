e98f8e2b7b6058937250a40427957345e3a8c346
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2014-09-18 15:22:14 +0900

    Adds a raw string literal feature.

--- src/main/scala/org/goldenport/sexpr/RawStringLiteralTransformer.scala
+++ src/main/scala/org/goldenport/sexpr/RawStringLiteralTransformer.scala
@@ -4,7 +4,7 @@ import com.asamioffice.goldenport.text.UJavaString
 
 /*
  * @since   Sep. 14, 2014
- * @version Sep. 14, 2014
+ * @version Sep. 18, 2014
  * @author  ASAMI, Tomoharu
  */
 object RawStringLiteralTransformer {
@@ -53,3 +53,51 @@ object RawStringLiteralTransformer {
     }
   }
 }
+
+// object RawStringLiteralTransformer2 {
+//   import scalaz._, Scalaz._
+
+//   type TransformState = State[St, Seq[String]]
+
+//   def transform(s: String): String = {
+//     val a = s.split("\"\"\"")
+//     if (a.length > 1) {
+//       val b = _transform(a)
+//       b.mkString
+//     } else {
+//       s
+//     }
+//   }
+
+//   private def _transform(xs: Seq[String]): Seq[String] = {
+//     val a: TransformState = State(s1 => (s1, s1.strings))
+//     val c = xs.foldLeft(a)((z, x) => z.flatMap(_add(x)))
+//     c
+//   }
+
+//   private def _add(x: String)(xs: Seq[String]): TransformState = {
+//     State(_.action(x))
+//   }
+
+//   sealed trait St {
+//     def strings: Vector[String]
+//     def action(s: String): (St, Vector[String])
+//   }
+
+//   case class ContentState(strings: Vector[String]) extends St {
+//     def action(s: String): (St, Vector[String]) = {
+//       val r = s
+//       val xs = strings :+ r
+//       (RawState(xs), xs)
+//     }
+//   }
+
+//   case class RawState(strings: Vector[String]) extends St {
+//     def action(s: String): (St, Vector[String]) = {
+//       val a = UJavaString.escapeJavaText(s)
+//       val r = "\"" + a + "\""
+//       val xs = strings :+ r
+//       (ContentState(xs), xs)
+//     }
+//   }
+// }
